2fa6a177833d91388ba87c18f3856964a52bafb0
Nicholas L <33159891+EKKOING@users.noreply.github.com>
2019-08-25 23:48:32 -0400

    Fix Domain Error

--- scheduler/js/scheduler.js
+++ scheduler/js/scheduler.js
@@ -22,8 +22,8 @@ function putCookie()
 	var classes = ["m1","m2","m3","m4","m5","m6","m7","m8","t1","t2","t3","t4","t5","t6","t7","t8","w1","w2","w3","w4","y1","y2","y3","y4","y5","y6","y7","y8","f1","f2","f3","f4","f5","f6","f7","f8","s1","s2","s3","s4"];
 
 	classes.forEach(bakeCookies);
-	console.log (boiyardee.join(";") + ";domain=domain=ekkoing.github.io/cclock/;max-age=31536000");
-	document.cookie = boiyardee.join(";") + ";domain=domain=ekkoing.github.io/cclock/;max-age=31536000";
+	console.log (boiyardee.join(";") + ";domain=ekkoing.github.io/cclock/;max-age=31536000");
+	document.cookie = boiyardee.join(";") + ";domain=ekkoing.github.io/cclock/;max-age=31536000";
 	return true;
 }
 
